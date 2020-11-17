ad62bd56d94af32afeb5540b735c12c56b91bde9
andy <kamsvaag.anders@gmail.com>
2020-11-18 00:11:35 +0100

    Fixed pre-commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,11 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
