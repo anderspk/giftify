9e2bc0d69b48f580aa625b62099285794dfc6261
andy <kamsvaag.anders@gmail.com>
2020-11-17 23:52:27 +0100

    Setup boilerplate

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import '../styles/globals.css';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
